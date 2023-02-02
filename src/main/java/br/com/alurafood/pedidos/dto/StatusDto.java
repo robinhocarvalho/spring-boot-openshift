c140a239cc25d67e73a1b0fe5f68c665b75ac2b3
Robson Carvalho <uni.robson@gmail.com>
2023-02-02 09:25:58 -0300

    Initial commit

---
+++ src/main/java/br/com/alurafood/pedidos/dto/StatusDto.java
@@ -0,0 +1,16 @@
+package br.com.alurafood.pedidos.dto;
+
+import br.com.alurafood.pedidos.model.Status;
+
+public class StatusDto {
+   
+	private Status status;
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+}
